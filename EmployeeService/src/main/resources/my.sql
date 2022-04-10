861d920f12a57727952890c4147736bccae182fe
Rahul <rnnehere@gmail.com>
2022-04-10 23:38:24 +0530

    First Commit

---
+++ EmployeeService/src/main/resources/my.sql
@@ -0,0 +1,3 @@
+ insert into roles values('1','ADMIN');
+ INSERT INTO USERS VALUES('1','$2a$12$nkdHFKltl8X6jkrM6vKDru1RsR0Py4o9KYCoz6fgxChRUXRgS9X.6','user1');
+ insert into users_roles values(1,1);
